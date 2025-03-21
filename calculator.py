8d550e0393afed92f6801ed83f4b07547447ac1e
Enock Tarei <151881239+youngtarei@users.noreply.github.com>
2025-03-21 12:14:13 +0300

    A Calculator

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# Creating a simple calculator
+def calculator():
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operation = input("Enter the operation (+, -, *, /): ")
+        
+        if operation == '+':
+            result = num1 + num2
+            print(f"{num1} + {num2} = {result}")
+        elif operation == '-':
+            result = num1 - num2
+            print(f"{num1} - {num2} = {result}")
+        elif operation == '*':
+            result = num1 * num2
+            print(f"{num1} * {num2} = {result}")
+        elif operation == '/':
+            if num2 != 0:
+                result = num1 / num2
+                print(f"{num1} / {num2} = {result}")
+            else:
+                print("Error: Division by zero is not allowed.")
+        else:
+            print("Invalid operation. Please enter one of +, -, *, /")
+
+calculator()
